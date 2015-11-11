e6530fbee2ae055fdb586f955f4bca14265c1cf0
prestonen <p.engstrom94@gmail.com>
2015-11-11 18:55:57 -0500

    added board.h why was that missing?

---
+++ the-latest-greatest-ai-game/Board.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <iostream>
+
+class Board{
+public:
+	///default constructor
+	Board::Board();
+
+	///<summary>
+	///Construct a new board idnetical to this one
+	///</summary>
+	///<returns>Deep copy of this Board</returns>
+	Board::Board(Board& genetic);
+
+	///Write a representation of the state to
+	///console
+	void Board::printState();
+
+	///<summary>
+	///drop the last row 
+	///</summary>
+	void Board::tetrisDrop();
+
+	///<summary>
+	///Attempt to drop a piece into column col, based on player indicated by turn
+	///</summary>
+	///<param name="col"> the column attempting to move in </param>
+	///<param name="turn"> flag indicating whose turn it is </param>
+	///<returns>True if move is applied to the Board, False otherwise</returns>
+	bool Board::applyMove(unsigned int col, bool turn);
+
+	///<summary>
+	///Tests the board for a string of 4, with horizontal wrapping
+	///</summary>
+	bool Board::testWin();
+
+private:
+	///used to indicate min or max
+	bool _play_state;
+
+	///Board itself
+	unsigned char state[8][8];
+
+	///if columns are full
+	unsigned short col_fill[8];
+};
+
